3f5e6b1768e6cfa02152d196493b67561d73b4f4
shifangyan <852712053@qq.com>
2017-11-20 14:39:29 +0800

    添加了服务器记录

--- unix_environment_programming/homework_4/test.cpp
+++ unix_environment_programming/homework_4/test.cpp
@@ -26,24 +26,27 @@ private:
     int result_;
 };
 
-AddTask::AddTask()
+AddTask::AddTask() 
 {
     add_num1_=0;
     add_num2_=0;
     result_= 0;
 }
 
+//设置加数
 void AddTask::SetAddNum(int num1,int num2)
 {
     add_num1_ = num1;
     add_num2_ = num2;
 }
 
+//任务运行函数
 void AddTask::TaskFunction()
 {
     result_ = add_num1_+add_num2_;
 }
 
+//打印加法结果
 void AddTask::PrintResult()
 {
     std::cout << "task ID:" << task_ID_ 
@@ -57,10 +60,10 @@ AddTask::~AddTask()
 
 int main(int argc,char *argv[])
 {
-    const int kTaskNum = 1000;
-    ThreadPoolManagerment::GetInstance()->SetThreadNum(10);
-    ThreadPoolManagerment::GetInstance()->Run();
-    for(int i = 0;i < kTaskNum ;++i)
+    const int kTaskNum = 1000;   //加法任务数量
+    ThreadPoolManagerment::GetInstance()->SetThreadNum(10);  //线程池运行线程数量
+    ThreadPoolManagerment::GetInstance()->Run();  //启动线程池
+    for(int i = 0;i < kTaskNum ;++i)  //循环创建加法任务 并放入线程池
     {
         AddTask *add_task = TaskManagerment::GetInstance()->CreatTask<AddTask>();
         add_task->SetAddNum(i,i);
@@ -68,7 +71,7 @@ int main(int argc,char *argv[])
     }
 
     int num = 0;
-    for(int i = 0;i < kTaskNum;++i)
+    for(int i = 0;i < kTaskNum;++i)  //循环接收已完成任务 如果任务数量达到创建任务数量 打印标志
     {
         Task *task = ThreadPoolManagerment::GetInstance()->GetTask();
         task->PrintResult();
